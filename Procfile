28dbfcecea008c6fec7719d5da9f12311946238b
Ильдар Абдулабаев <gamedozfeed@gmail.com>
2019-11-09 17:30:50 +0600

    Deploy

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn newsparser.wsgi --log-file -
--- newsparser/settings.py
+++ newsparser/settings.py
@@ -120,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+import dj_database_url
+db_form_env = dj_database_url.config()
+DATABASES['default'].update(db_form_env)
